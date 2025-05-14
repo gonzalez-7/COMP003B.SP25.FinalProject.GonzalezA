bea4aef1dcc84a17230c7fb6b478cadc3ba0dcd6
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-14 16:19:41 -0700

    Added a readme and configure the program.cs for the swagger

--- COMP003B.SP25.FinalProject.GonzalezA/Program.cs
+++ COMP003B.SP25.FinalProject.GonzalezA/Program.cs
@@ -1,3 +1,8 @@
+// Author: Antonio Gonzalez
+// Course: COMP003B
+// Instructor: Jonathan Cruz
+// Purpose: Final project synthesizing MVC, Web API, EF Core, and middleware
+
 using COMP003B.SP25.FinalProject.GonzalezA.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +14,22 @@ namespace COMP003B.SP25.FinalProject.GonzalezA
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
 			// Database Context
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 			var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
