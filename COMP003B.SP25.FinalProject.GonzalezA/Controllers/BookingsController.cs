03834c9d7342194b2629cc36097ad04ae9850741
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-14 15:37:26 -0700

    Created controller with views for the model classes

---
+++ COMP003B.SP25.FinalProject.GonzalezA/Controllers/BookingsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP003B.SP25.FinalProject.GonzalezA.Data;
+using COMP003B.SP25.FinalProject.GonzalezA.Models;
+
+namespace COMP003B.SP25.FinalProject.GonzalezA.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bookings
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Bookings.Include(b => b.Car).Include(b => b.Customer).Include(b => b.Service);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Bookings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Car)
+                .Include(b => b.Customer)
+                .Include(b => b.Service)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // GET: Bookings/Create
+        public IActionResult Create()
+        {
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Make");
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email");
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "Id");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,BookingDate,CustomerId,CarId,ServiceId,Notes")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Make", booking.CarId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", booking.CustomerId);
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "Id", booking.ServiceId);
+            return View(booking);
+        }
+
+        // GET: Bookings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Make", booking.CarId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", booking.CustomerId);
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "Id", booking.ServiceId);
+            return View(booking);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BookingDate,CustomerId,CarId,ServiceId,Notes")] Booking booking)
+        {
+            if (id != booking.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(booking);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingExists(booking.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "Id", "Make", booking.CarId);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", booking.CustomerId);
+            ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "Id", booking.ServiceId);
+            return View(booking);
+        }
+
+        // GET: Bookings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .Include(b => b.Car)
+                .Include(b => b.Customer)
+                .Include(b => b.Service)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking != null)
+            {
+                _context.Bookings.Remove(booking);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookingExists(int id)
+        {
+            return _context.Bookings.Any(e => e.Id == id);
+        }
+    }
+}
