cd7d9b90088590079d815f7721991e558e285005
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-14 14:19:39 -0700

    Created a Data folder to add ApplicationDbContext

---
+++ COMP003B.SP25.FinalProject.GonzalezA/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.SP25.FinalProject.GonzalezA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.GonzalezA.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options) { }
+
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Booking> Bookings { get; set; }
+		public DbSet<ServiceType> ServiceTypes { get; set; }
+	}
+}
