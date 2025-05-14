f0976f878808e8f94d40e346aa13e85ecda80a31
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-14 14:42:34 -0700

    Setup the program.cs and the secrets.json

--- COMP003B.SP25.FinalProject.GonzalezA/Program.cs
+++ COMP003B.SP25.FinalProject.GonzalezA/Program.cs
@@ -14,7 +14,7 @@ namespace COMP003B.SP25.FinalProject.GonzalezA
 
 			// Database Context
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 			var app = builder.Build();
 
