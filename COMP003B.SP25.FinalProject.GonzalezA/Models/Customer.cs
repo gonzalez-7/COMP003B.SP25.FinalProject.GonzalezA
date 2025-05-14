211777dcbdfd203aa4fe60bcea973bde54db0062
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-14 14:14:03 -0700

    Added Info to the Customer.cs located in the folder Models

--- COMP003B.SP25.FinalProject.GonzalezA/Models/Customer.cs
+++ COMP003B.SP25.FinalProject.GonzalezA/Models/Customer.cs
@@ -1,6 +1,20 @@
-﻿namespace COMP003B.SP25.FinalProject.GonzalezA.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.SP25.FinalProject.GonzalezA.Models
 {
 	public class Customer
 	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string FullName { get; set; }
+
+		[Required]
+		public string Email { get; set; }
+
+		public string Phone { get; set; }
+
+		public ICollection<Booking> Bookings { get; set; }
 	}
 }
